b4452ded4a3a851764d129753480a057bb6587a2
Álex Román Núñez <eirexe123@gmail.com>
2020-09-27 00:52:30 +0200

    More shit

--- src/audio_normalizer.rs
+++ src/audio_normalizer.rs
@@ -2,20 +2,27 @@ use super::pool_byte_array::PoolByteArray;
 use ebur128::EbuR128;
 use gdnative::prelude::*;
 use lewton::inside_ogg::OggStreamReader;
+use std::time::Instant;
 #[derive(NativeClass, Default)]
 #[inherit(Reference)]
-pub struct AudioNormalizer {}
+pub struct AudioNormalizer {
+    target_loudness: f64,
+}
 
 impl AudioNormalizer {
     fn new(_owner: &Reference) -> Self {
-        AudioNormalizer {}
+        AudioNormalizer {
+            target_loudness: -24.0,
+        }
     }
 }
-
-const TARGET_VOLUME: f64 = -24.0;
-
 #[methods]
 impl AudioNormalizer {
+    #[export]
+    pub fn set_target_loudness(&mut self, _owner: &Reference, target_loudness: f64) {
+        self.target_loudness = target_loudness
+    }
+
     /// Returns global LUFS loudness for the given OGG file data
     #[export]
     pub fn get_loudness_gobal(&mut self, _owner: &Reference, stream: ByteArray) -> f64 {
@@ -40,20 +47,22 @@ impl AudioNormalizer {
                             "Unsupported number of channels in provided file {:?}, expected 1 or 2",
                             reader.ident_hdr.audio_channels
                         );
-                        return TARGET_VOLUME;
+                        return self.target_loudness;
                     }
                 }
-
+                let start = Instant::now();
                 while let Some(mut pck_samples) = reader.read_dec_packet_itl().unwrap() {
                     ebu.add_frames_i16(&mut pck_samples).unwrap();
                 }
-
-                return ebu.loudness_global().unwrap();
+                let loudness = ebu.loudness_global().unwrap();
+                let duration = start.elapsed().as_secs();
+                godot_print!("Finished processing samples, took {} seconds.", duration);
+                return loudness;
             }
             Err(err) => {
                 godot_print!("Error loading OGG file for normalization {:?}", err);
             }
         }
-        TARGET_VOLUME
+        self.target_loudness
     }
 }
